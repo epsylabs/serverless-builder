1ff75877ca804e74861e3492fca5e1bd08fefd2a
Daniel Ancuta <whisller@gmail.com>
2022-01-26 15:03:59 +0100

    Added `EventDispatcherDLQ`
    Class that wraps our `event-dispatcher`.

--- serverless/aws/features/__init__.py
+++ serverless/aws/features/__init__.py
@@ -1,2 +1,3 @@
 from .xray import XRay
 from .api_handler import DefaultFourHundredResponse
+from .event_dispatcher import DLQ as EventDispatcherDLQ
---
+++ serverless/aws/features/event_dispatcher.py
@@ -0,0 +1,31 @@
+from troposphere import GetAtt
+from troposphere.cloudwatch import Alarm, MetricDimension
+from troposphere.sqs import Queue
+
+from serverless.service.types import Feature
+
+
+class DLQ(Feature):
+    def enable(self, service):
+        service.resources.add(
+            Queue(
+                "EventDispatcherDLQ",
+                QueueName="${self:service}-event-dispatcher-dlq",
+                MessageRetentionPeriod=1209600  # 14 days in seconds
+            )
+        )
+        service.resources.add(
+            Alarm(
+                "EventDispatcherDLQCloudWatchAlarm",
+                AlarmDescription="Unable to send event from handler.",
+                AlarmActions=["arn:aws:sns:${aws:region}:${aws:accountId}:foxglove-${self:custom.stage}-cloudwatch-alerts"],
+                Namespace="AWS/SQS",
+                MetricName="ApproximateNumberOfMessagesVisible",
+                Dimensions=[MetricDimension(Name="QueueName", Value=GetAtt("EventDispatcherDLQ", "QueueName"))],
+                Statistic="Sum",
+                Period=60,
+                EvaluationPeriods=1,
+                Threshold=1,
+                ComparisonOperator="GreaterThanOrEqualToThreshold",
+            )
+        )
