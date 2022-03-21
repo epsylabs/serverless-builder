cfb415a00b4d73517db03880fbb50ca5302b92bb
Michal Przytulski <michal@przytulski.pl>
2022-03-21 13:20:52 +0100

    add code signing

--- serverless/service/plugins/code_sign.py
+++ serverless/service/plugins/code_sign.py
@@ -10,7 +10,7 @@ class AWSCodeSign(Generic):
         sign_policy="Enforce",
         source_bucket=None,
         source_key=None,
-        desitnation_bucket=None,
+        destination_bucket=None,
         destination_prefix=None,
         retain=False,
     ):
@@ -19,15 +19,25 @@ class AWSCodeSign(Generic):
         self.sign_policy = sign_policy
         self.source_bucket = source_bucket
         self.source_key = source_key
-        self.destination_bucket = desitnation_bucket
-        self.destination_prefix = destination_prefix
+        self.destination_bucket = destination_bucket
+        self.destination_prefix = destination_prefix or "signed-"
         self.retain = retain
 
     def enable(self, service):
         service.custom.signer = dict(
             retain=self.retain,
-            source=dict(s3=dict(bucketName=self.source_bucket, key=self.source_key)),
-            destination=dict(s3=dict(bucketName=self.destination_bucket, prefix=self.destination_prefix)),
+            source=dict(
+                s3=dict(
+                    bucketName=self.source_bucket or service.provider.deploymentBucket.get("name"),
+                    key=self.source_key or f"{service.service.spinal}.zip",
+                )
+            ),
+            destination=dict(
+                s3=dict(
+                    bucketName=self.destination_bucket or service.provider.deploymentBucket.get("name"),
+                    prefix=self.destination_prefix,
+                )
+            ),
             profileName=self.profile,
             signingPolicy=self.sign_policy,
         )
