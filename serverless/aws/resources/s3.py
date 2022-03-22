73a5efbee11048f0552568ffbd2aac4e6b50ab74
Karol Czeryna <k@e-dot.uk>
2022-03-22 15:01:30 +0000

    Work on S3 with dynamic bucketName

---
+++ serverless/aws/resources/s3.py
@@ -0,0 +1,19 @@
+from troposphere.s3 import Bucket, Private
+from serverless.aws.resources import Resource
+# from serverless.aws.resources.kms import EncryptableResource
+from serverless.service import Identifier
+
+
+class Bucket(Resource):
+    def __init__(self, BucketName, **kwargs):
+        self.bucket = Bucket(
+            title=Identifier(BucketName, safe=True).pascal,
+            BucketName=Identifier(BucketName, safe=True).pascal,
+            **kwargs
+        )
+
+        if "${self:service}" not in BucketName:
+            self.bucket.properties.__setitem__("BucketName", "${self:service}-${sls:stage}-${aws:region}" + BucketName)
+
+    def resources(self):
+        return super().resources() + [self.bucket]
