db589686ca0771d7ed376dba74db3009dd45647c
Michal Przytulski <michal@przytulski.pl>
2022-03-24 15:07:40 +0100

    fix LogGroup

--- serverless/aws/resources/kms.py
+++ serverless/aws/resources/kms.py
@@ -4,3 +4,6 @@ from troposphere import Ref
 class EncryptableResource:
     def encryption_key(self):
         return Ref("ServiceEncryptionKey")
+
+    def encryption_alias(self):
+        return "alias/${self:service}-${sls:stage}"
--- serverless/aws/resources/logs.py
+++ serverless/aws/resources/logs.py
@@ -8,7 +8,7 @@ from serverless.service.types import Identifier
 class LogGroup(Resource, EncryptableResource):
     def __init__(self, LogGroupName, **kwargs) -> None:
         kwargs.setdefault("RetentionInDays", 30)
-        kwargs.setdefault("KmsKeyId", self.encryption_key())
-        kwargs.setdefault("title", Identifier(LogGroupName).resource)
+        kwargs.setdefault("KmsKeyId", self.encryption_alias())
+        kwargs.setdefault("title", Identifier(LogGroupName + "LogGroup").resource)
 
         super().__init__(AWSLogGroup(LogGroupName=LogGroupName, **kwargs))
