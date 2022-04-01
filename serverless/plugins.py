9437a5ea0d7ae5bb5897f3b08e33dcd6a625082b
David Szlovak <david.szlovak@epsyhealth.com>
2022-04-01 16:06:22 +0200

    Localstack plugin

--- serverless/plugins.py
+++ serverless/plugins.py
@@ -2,6 +2,7 @@ from serverless.service.plugins.composed_vars import ComposedVars
 from serverless.service.plugins.deployment_bucket import DeploymentBucket
 from serverless.service.plugins.domain_manager import DomainManager
 from serverless.service.plugins.generic import Generic
+from serverless.service.plugins.localstack import Localstack
 from serverless.service.plugins.prune import Prune
 from serverless.service.plugins.python_requirements import PythonRequirements
 from serverless.service.plugins.step_functions import StepFunctions
---
+++ serverless/service/plugins/localstack.py
@@ -0,0 +1,26 @@
+from serverless.service.plugins.generic import Generic
+
+
+class Localstack(Generic):
+    yaml_tag = "!LocalstackPlugin"
+
+    def __init__(self, stages=["local"], host="http://localhost", edgePort=4566, autostart=True, networks=[], mountCode=False, sudo=False):
+        super().__init__("serverless-localstack")
+        self.stages = stages
+        self.host = host
+        self.edgePort = edgePort
+        self.autostart = autostart
+        self.networks = networks
+        self.lambda_settings = dict(mountCode=mountCode)
+        self.docker = dict(sudo= sudo)
+
+
+    def enable(self, service):
+        export = dict(self)
+        export.pop("name", None)
+
+        export["lambda"] = export.get("lambda_settings")
+        export.pop("lambda_settings", None)
+
+        service.custom.localstack = export
+        service.custom.stages = {stage: {} for stage in self.stages}
