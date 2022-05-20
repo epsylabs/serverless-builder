af96744bed8049b4c4bb11bc71da29b28b81ddac
Michal Przytulski <michal@przytulski.pl>
2022-05-20 12:45:55 +0200

    extra resources

--- serverless/aws/types.py
+++ serverless/aws/types.py
@@ -1,3 +1,5 @@
+import yaml
+
 from serverless.service import YamlOrderedDict
 
 
@@ -17,6 +19,27 @@ class SQSArn(GenericArn):
         return f"arn:aws:sqs:${{aws:region}}:${{aws:accountId}}:{self.name}"
 
 
+class DynamoDBArn(GenericArn):
+    yaml_tag = "!Arn"
+
+    def __init__(self, table):
+        self.table = table
+
+    def __str__(self):
+        return f"arn:aws:dynamodb:${{aws:region}}:${{aws:accountId}}:table/{self.table.TableName}"
+
+
+class DynamoDBIndexArn(GenericArn):
+    yaml_tag = "!Arn"
+
+    def __init__(self, table, index="*"):
+        self.table = table
+        self.index = index
+
+    def __str__(self):
+        return f"arn:aws:dynamodb:${{aws:region}}:${{aws:accountId}}:table/{self.table.TableName}/index/{self.index}"
+
+
 class EventBridgeBusArn(GenericArn):
     yaml_tag = "!Arn"
 
@@ -25,3 +48,26 @@ class EventBridgeBusArn(GenericArn):
 
     def __str__(self):
         return f"arn:aws:events:${{aws:region}}:${{aws:accountId}}:event-bus/{self.name}"
+
+
+class Ref(yaml.YAMLObject):
+    yaml_tag = "!Ref"
+
+    def __init__(self, data):
+        self.data = data
+
+    @classmethod
+    def to_yaml(cls, dumper, data):
+        return dumper.represent_dict({"Ref": data.data})
+
+
+class Equals(yaml.YAMLObject):
+    yaml_tag = "!Equals"
+
+    def __init__(self, title, data):
+        self.title = title
+        self.data = data
+
+    @classmethod
+    def to_yaml(cls, dumper, data):
+        return dumper.represent_dict({"Fn::Equals": data.data})
